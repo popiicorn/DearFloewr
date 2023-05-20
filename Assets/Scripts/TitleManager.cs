7b4d8ea5648a80c85895018c95d20d677b421d01
TsunehikoShimazu <simanezumi1989@gmail.com>
2023-05-21 02:22:41 +0900

    Fade導入

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    bool isStart;
+
+    public void OnStart()
+    {
+        if (isStart)
+        {
+            return;
+        }
+        isStart = true;
+        FadeManager.Instance.LoadScene("Main", 1.5f);
+    }
+}
