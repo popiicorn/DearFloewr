a9c58d906b24dd87ac1e725d33deac660358e99e
TsunehikoShimazu <simanezumi1989@gmail.com>
2023-07-24 19:25:12 +0900

    エモーションの設定

--- Assets/Scripts/Stage3/Scrap.cs
+++ Assets/Scripts/Stage3/Scrap.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Scrap : MonoBehaviour
 {
-    // プレイヤが到着したら実行したいこと => エモーションを出す
+    [SerializeField] float timeOfActive;
+    [SerializeField] GameObject emotion;
+    WaitForSeconds wait;
+    private void Awake()
+    {
+        wait = new WaitForSeconds(timeOfActive);
+    }
+    public void Show()
+    {
+        StopAllCoroutines();
+        StartCoroutine(ShowCor());
+    }
+
+    IEnumerator ShowCor()
+    {
+        emotion.SetActive(true);
+        yield return wait;
+        emotion.SetActive(false);
+    }
 }
