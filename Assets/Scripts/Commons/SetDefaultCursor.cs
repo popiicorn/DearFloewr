d7c67a0dab13c36882de73ccef15c10fcf1e8eec
TsunehikoShimazu <simanezumi1989@gmail.com>
2023-10-30 15:18:21 +0900

    カーソルサイズが小さくなるバグの修正

--- Assets/Scripts/Commons/SetDefaultCursor.cs
+++ Assets/Scripts/Commons/SetDefaultCursor.cs
@@ -3,14 +3,25 @@ using UnityEngine;
 public class SetDefaultCursor : MonoBehaviour
 {
     [SerializeField] Texture2D icon;
-
+    int size;
     private void Awake()
     {
+        size = ((57*100*Screen.width)/1920)/100;
         SetCursor(icon);
     }
-
+    private void Update()
+    {
+    }
     public void SetCursor(Texture2D icon)
     {
-        Cursor.SetCursor(icon, Vector2.zero, CursorMode.ForceSoftware);
+        // Cursor.SetCursor(icon, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(ResizeTexture(icon, size,size), Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    Texture2D ResizeTexture(Texture2D srcTexture, int newWidth, int newHeight)
+    {
+        var resizedTexture = new Texture2D(newWidth, newHeight, TextureFormat.RGBA32, false);
+        Graphics.ConvertTexture(srcTexture, resizedTexture);
+        return resizedTexture;
     }
 }
