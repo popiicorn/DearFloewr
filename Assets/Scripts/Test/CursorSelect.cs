8d7eae76e08e0ee98cf548ff20176d12ede7a772
TsunehikoShimazu <simanezumi1989@gmail.com>
2023-11-01 09:19:25 +0900

    カーソル修正

--- Assets/Scripts/Test/CursorSelect.cs
+++ Assets/Scripts/Test/CursorSelect.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class CursorSelect : MonoBehaviour
 {
+    int size;
+
+    private void Start()
+    {
+        size = ((57 * 100 * Screen.width) / 1920) / 100;
+    }
     public void SetCursor(Texture2D icon)
     {
-        Cursor.SetCursor(icon, Vector2.zero, CursorMode.ForceSoftware);
+        // Cursor.SetCursor(icon, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(ResizeTexture(icon, size, size), Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    Texture2D ResizeTexture(Texture2D srcTexture, int newWidth, int newHeight)
+    {
+        var resizedTexture = new Texture2D(newWidth, newHeight, TextureFormat.RGBA32, false);
+        Graphics.ConvertTexture(srcTexture, resizedTexture);
+        return resizedTexture;
     }
 }
