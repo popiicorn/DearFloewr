b74a99f01e5c0ebe022423a636921a3309c965a4
TsunehikoShimazu <simanezumi1989@gmail.com>
2024-05-28 20:42:44 +0900

    Update GS36_EnemyAttack.cs

--- Assets/Scripts/GS33/GS36_EnemyAttack.cs
+++ Assets/Scripts/GS33/GS36_EnemyAttack.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using DG.Tweening;
-using UnityEditor.Rendering.LookDev;
 public class GS36_EnemyAttack : MonoBehaviour
 {
     [SerializeField] bool debugMode;
