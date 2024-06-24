f7966b6d60bf9ed6a7fd63f3edb37f88153f4f43
TsunehikoShimazu <simanezumi1989@gmail.com>
2024-06-24 11:20:17 +0900

    シーン遷移の自動化

--- Assets/Scripts/Commons/TransitionSceneManager.cs
+++ Assets/Scripts/Commons/TransitionSceneManager.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class TransitionSceneManager : MonoBehaviour
 {
+    [SerializeField] List<EventData> transitionEvents = new List<EventData>();
     public void TransitionScene()
     {
         string sceneName = GameStageManager.Instance.GetNextStageName();
         SceneManager.LoadScene(sceneName);
         // FadeManager.Instance.LoadScene(sceneName, 1.0f);
+    }
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    public void OnTransitionAction()
+    {
+        StartCoroutine(PlayAnim());
+    }
+
+    IEnumerator PlayAnim()
+    {
+        foreach (EventData eventData in transitionEvents)
+        {
+            yield return eventData.Play();
+        }
+        Cursor.visible = true;
     }
 }
