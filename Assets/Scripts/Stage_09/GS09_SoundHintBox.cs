a225f2b490e4b6f722ef2a4fa014a207cdf46002
TsunehikoShimazu <simanezumi1989@gmail.com>
2024-09-21 15:20:53 +0900

    Stage11 HintBox

---
+++ Assets/Scripts/Stage_09/GS09_SoundHintBox.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GS09_SoundHintBox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("HintBox Triggered");
+    }
+}
