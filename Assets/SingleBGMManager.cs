ff260d5903b65a1deed9a037aa9f7dd4c0489b08
kuwapo <83585233+kuwapo@users.noreply.github.com>
2024-09-28 00:29:11 +0900

    SE追加

---
+++ Assets/SingleBGMManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CriWare;
+using DG.Tweening;
+
+
+public class SingleBGMManager : MonoBehaviour
+{
+    public CriAtomSource bGMAtomSource;
+    public string bGMCueName;
+
+    public void StartBGM(string cueName)
+    {
+        bGMAtomSource.Play(cueName);
+    }
+
+    public void StopBGM()
+    {
+
+        bGMAtomSource.Stop();
+    }
+
+    public void OffAisac()
+    {
+        DOVirtual.Float(0.7f, 0f, 3f, value =>
+        {
+            bGMAtomSource.SetAisacControl("Filter", value);
+        });
+
+    }
+
+    public void OnBGMAisac(string aisacName)
+    {
+        DOVirtual.Float(0f, 1f, 1f, value =>
+        {
+            bGMAtomSource.SetAisacControl(aisacName, value);
+        });
+    }
+
+
+    }
