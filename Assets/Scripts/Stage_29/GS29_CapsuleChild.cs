ff9aafc27c6c1f7de26f9032e98144076aa3bcb0
TsunehikoShimazu <simanezumi1989@gmail.com>
2024-10-10 13:51:16 +0900

    転がる音の実装

---
+++ Assets/Scripts/Stage_29/GS29_CapsuleChild.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GS29_CapsuleChild : MonoBehaviour
+{
+    int count = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        count++;
+        if (count < 5)
+        {
+            return;
+        }
+        CriManager.instance.PlayObjSE("CupcellHop");
+    }
+}
