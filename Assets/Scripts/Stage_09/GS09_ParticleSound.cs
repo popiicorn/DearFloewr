5349e3a45107a724bf388cc0dffc2a991cc285aa
kuwapo <83585233+kuwapo@users.noreply.github.com>
2024-10-11 01:33:08 +0900

    ST11SE修正

--- Assets/Scripts/Stage_09/GS09_ParticleSound.cs
+++ Assets/Scripts/Stage_09/GS09_ParticleSound.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class GS09_ParticleSound : MonoBehaviour
 {
+    bool gameClear = false;
     private void OnParticleCollision(GameObject other)
     {
-        if (other.CompareTag("WaterGround"))
+        if (other.CompareTag("WaterGround") && gameClear == false)
         {
+
             Debug.Log("hit");
+            CriManager.instance.Play3D();
 
         }
     }
+
+    public void stopDropSound()
+    {
+        gameClear = true;
+    }
+
 }
