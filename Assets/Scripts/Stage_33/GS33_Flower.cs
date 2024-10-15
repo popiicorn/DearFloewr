a6871ca038fe826f7791314ae2e80243d7396a4e
TsunehikoShimazu <simanezumi1989@gmail.com>
2024-10-15 09:46:38 +0900

    花びら連打防止

--- Assets/Scripts/Stage_33/GS33_Flower.cs
+++ Assets/Scripts/Stage_33/GS33_Flower.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 using UnityEngine.Events;
 public class GS33_Flower : MonoBehaviour
 {
+    bool isGet = false;
     // クリックされるとエフェクトを再生
     public UnityAction OnClickAction;
 
     public void OnClick()
     {
+        if (isGet)
+        {
+            return;
+        }
+        isGet = true;
         OnClickAction?.Invoke();
         CriManager.instance.PlayObjSE("GetPetal");
     }
