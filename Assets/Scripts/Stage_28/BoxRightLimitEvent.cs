22873c9a07b3c15fab0d0fd54a073194cc64d62f
TsunehikoShimazu <simanezumi1989@gmail.com>
2024-10-15 10:03:26 +0900

    Box音の修正

--- Assets/Scripts/Stage_28/BoxRightLimitEvent.cs
+++ Assets/Scripts/Stage_28/BoxRightLimitEvent.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoxRightLimitEvent : MonoBehaviour
 {
     [SerializeField] Gimmick gimmick;
     [SerializeField] List<EventData> eventDatas;
+    [SerializeField] bool canSound = false;
     // Start is called before the first frame update
     void Start()
     {
-        gimmick.OnLimit += OnLimit;
+        // gimmick.OnLimit += OnLimit;
     }
 
     void OnLimit()
@@ -23,6 +25,20 @@ public class BoxRightLimitEvent : MonoBehaviour
         foreach (var eventData in eventDatas)
         {
             yield return eventData.Play();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<GS28_Box>() || collision.GetComponent<GS29_Box>())
+        {
+            if (!canSound)
+            {
+                canSound = true;
+                return;
+            }
+            OnLimit();
+            Debug.Log("OnTriggerEnter2D" + collision.name);
         }
     }
 }
